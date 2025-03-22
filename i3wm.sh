e0e2abcde7f864e9df8e374b4a4aacd0b57e864c
SSMassociados <ssmassociados@hotmail.com>
2025-03-22 09:41:38 -0300

    Install I3wm

---
+++ i3wm.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# Configuração básica do sistema
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+# Fstrim
+sudo systemctl enable --now fstrim.timer
+
+# Habilitando repositório Multilib
+sudo sed -i '/multilib\]/,+1 s/^#//' /etc/pacman.conf && sudo pacman -Sy
+
+# Atualizar mirrorlist para obter os melhores mirrors
+sudo pacman -S --noconfirm --needed reflector
+systemctl enable --now reflector.timer
+sudo reflector -l 7 -a 24 -p https --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
+
+# Configuração do firewall (opcional)
+#sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+#sudo firewall-cmd --add-port=1025-65535/udp --permanent
+#sudo firewall-cmd --reload
+
+# Instalação do YAY (helper AUR)
+# cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg --noconfirm -s && sudo pacman -U --noconfirm yay*.pkg.tar.zst && cd .. && rm -rf yay
+
+# Instalação do YAY (helper AUR)
+if ! command -v yay &> /dev/null; then
+    cd /tmp
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg --noconfirm -s
+    sudo pacman -U --noconfirm yay*.pkg.tar.zst
+    cd ..
+    rm -rf yay
+fi
+
+# Instalação do Xorg e i3wm
+sudo pacman -S --noconfirm --needed xorg xorg-xinit i3-wm dmenu feh rofi rofi-emoji picom dunst autorandr polybar brightnessctl playerctl lxappearance-gtk3 mission-center xclip xdg-desktop-portal-gnome mugshot numlockx python-pywal qt5ct 
+
+# Instalação de aplicações essenciais
+sudo pacman -S --noconfirm --needed kitty kitty-terminfo man-db man-pages-pt_br tmate thunar thunar-archive-plugin thunar-media-tags-plugin thunar-shares-plugin thunar-volman ranger firefox firefox-i18n-pt-br qbittorrent atril flameshot keepassxc geany neovim neofetch htop arc-gtk-theme arc-icon-theme mpv vlc amberol obs-studio 
+
+# Fontes para melhorar a aparência e compatibilidade
+sudo pacman -S --noconfirm --needed ttf-dejavu ttf-liberation ttf-ubuntu-font-family ttf-fira-code ttf-font-awesome noto-fonts noto-fonts-emoji
+
+# Suporte a sistema de arquivos
+sudo pacman -S --noconfirm --needed os-prober intel-ucode dosfstools mtools freetype2 libisoburn fuse2 ntfs-3g e2fsprogs gvfs-{mtp,gphoto2,afc,smb} udisks2 polkit-gnome ifuse
+
+# Compactadores e descompactadores
+sudo pacman -S --noconfirm --needed file-roller cabextract zip lhasa p7zip unrar unarchiver unzip unace xz arj unarj tar gzip bzip2 ncompress
+
+# Áudio
+sudo pacman -S --noconfirm --needed pipewire pipewire-{alsa,pulse,jack} wireplumber helvum pavucontrol sof-firmware
+
+# Codecs de mídia
+sudo pacman -S --noconfirm --needed gst-libav gst-plugins-{base,good,bad,ugly} gstreamer-vaapi x265 x264 dvd+rw-tools dvdauthor dvgrab libmad libde265 libdv libdvdcss libdvdread libdvdnav libvorbis lame faac faad2 flac a52dec 
+
+# Bluetooth
+sudo pacman -S --noconfirm --needed bluez bluez-utils blueman
+systemctl enable --now bluetooth
+
+# Impressão e scan
+sudo pacman -S --noconfirm --needed avahi nss-mdns cups cups-pdf libcups print-manager system-config-printer simple-scan
+
+# ADB & SCRCPY
+sudo pacman -S --noconfirm --needed scrcpy android-tools android-udev
+usermod -aG adbusers $USER
+
+# ZSH
+sudo pacman -S --noconfirm --needed zsh zsh-completions 
+sudo chsh -s /usr/bin/zsh $USER 
+
+# Ferramentas de desenvolvimento (opcional)
+sudo pacman -S --noconfirm --needed git base-devel python python-pip nodejs npm
+
+# Suíte de escritório
+sudo pacman -S --noconfirm --needed libreoffice-still libreoffice-still-pt-br jre8-openjdk libmythes breeze-gtk 
+
+# Lista de pacotes do AUR a serem instalados
+packages=(
+    "wps-office"
+    "wps-office-mime"
+    "ttf-wps-fonts"
+    "wps-office-mui-pt-br"
+    "wps-office-vero-pt-br"
+    "libtiff5"
+    "qt5-styleplugins"
+    "qt6ct"
+    "qt6-tools-desktop"
+    "betterlockscreen"
+    "xidlehook"
+    "simplescreenrecorder"
+    "pkexec"
+    "djmount"
+    "autoadb-git"
+    "android-sdk-platform-tools"
+    "mythes-pt-br"
+    "libreoffice-extension-languagetool"
+    "hunspell-pt-br"
+    "hyphen-pt-br"
+    "hyph_pt_BR-213"
+    "hsqldb2-java"
+    "thunar-custom-actions"    
+)
+
+# Compila os pacotes com o yay (sem instalar)
+for pkg in "${packages[@]}"; do
+    yay -S --noconfirm --builddir ~/.cache/yay --downloadonly "$pkg"
+done
+
+# Instala os pacotes compilados com pacman -U
+for pkg in "${packages[@]}"; do
+    sudo pacman -U --noconfirm ~/.cache/yay/"$pkg"/*.pkg.tar.zst
+done
+
+echo "Instalação concluída!"
+
+# LightDM (gerenciador de login leve)
+sudo pacman -S --noconfirm --needed --needed lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+sudo systemctl enable lightdm
+
+# Configuração do i3wm
+sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
+echo "exec i3" >> ~/.xinitrc
+chmod +x ~/.xinitrc
+
+# Mensagem final
+/bin/echo -e "\e[1;32mINSTALAÇÃO CONCLUÍDA! REINICIANDO EM 5 SEGUNDOS...\e[0m"
+#sleep 5
+#sudo reboot
