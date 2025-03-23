984db9a1cf8a1a7231550141817f341c2b4f2e20
SSMassociados <ssmassociados@hotmail.com>
2025-03-23 18:57:08 -0300

    Update V2

--- i3wm.sh
+++ i3wm.sh
@@ -119,11 +119,55 @@ packages=(
     "thunar-custom-actions"    
 )
 
-for pkg in "${AUR_PACKAGES[@]}"; do
-    yay -G --builddir ~/.cache/yay "$pkg"
-    makepkg --noconfirm -s -C -p ~/.cache/yay/"$pkg"/PKGBUILD
-    sudo pacman -U --noconfirm ~/.cache/yay/"$pkg"/*.pkg.tar.zst
+#!/bin/bash
+
+# Lista de pacotes do AUR a serem instalados
+packages=(
+    "wps-office"
+    "wps-office-mime"
+    "ttf-wps-fonts"
+    "wps-office-mui-pt-br"
+    "wps-office-vero-pt-br"
+    "libtiff5"
+    "qt5-styleplugins"
+    "qt6ct"
+    "qt6-tools-desktop"
+    "betterlockscreen"
+    "xidlehook"
+    "simplescreenrecorder"
+    "pkexec"
+    "djmount"
+    "autoadb-git"
+    "android-sdk-platform-tools"
+    "mythes-pt-br"
+    "libreoffice-extension-languagetool"
+    "hunspell-pt-br"
+    "hyphen-pt-br"
+    "hyph_pt_BR-213"
+    "hsqldb2-java"
+    "thunar-custom-actions"    
+)
+
+# Compila os pacotes com o yay (sem instalar, sem sudo)
+for pkg in "${packages[@]}"; do
+    yay -S --noconfirm --builddir ~/.cache/yay --downloadonly "$pkg"
+    if [ $? -ne 0 ]; then
+        echo "Erro ao compilar o pacote $pkg"
+        exit 1
+    fi
 done
+
+# Instala os pacotes compilados com pacman -U (com sudo)
+for pkg in "${packages[@]}"; do
+    sudo pacman -U --noconfirm ~/.cache/yay/"$pkg"/*.pkg.tar.zst
+    if [ $? -ne 0 ]; then
+        echo "Erro ao instalar o pacote $pkg"
+        exit 1
+    fi
+done
+
+# Limpeza do cache (opcional)
+rm -rf ~/.cache/yay/*
 
 # LightDM (gerenciador de login leve)
 sudo pacman -S --noconfirm --needed --needed lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
